b0e598405e284da79e0da8785743a4d581fc3e0a
max Icolumi <max77648@gmail.com>
2021-04-06 20:18:37 +0300

    Написал Класс Продукт, Сотридник, Склад, Расписание.

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -1,114 +1,170 @@
 #include <iostream>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
-class BinaryTree
+class Product
 {
-	private:
-	struct Node
+private:
+	string _name;
+	string _dateDelivery;
+
+
+public:
+	Product():_name(NULL), _dateDelivery(NULL)
+	{}
+
+	Product(string name, string dateDelivery)
 	{
-		public:
-		Node* Right;
-		Node* Left;
-		int value;
-
-		Node():Left(NULL), Right(NULL), value() {}
-		Node(int newValue):Left(NULL), Right(NULL), value(newValue) {}
-
-		friend ostream& operator <<(ostream& out, const Node& node)
-		{
-			out << node.value;
-			return out;
-		}
-	};
-
-	Node* _root;
-	long _countNodes;
-	public:
-	BinaryTree(): _root(NULL), _countNodes(0) {}
-
-	void Add(int value)
-	{
-		_root = AddNode(value, _root);
-		++_countNodes;
+		_name = name;
+		_dateDelivery = dateDelivery;
 	}
 
-	void Show()
+	string GetName()
 	{
-		ShowNode(_root);
+		return _name;
 	}
 
-	bool Contains(int value)
+	string GetDateDelivery()
 	{
-		return (_root == NULL) ? false : IsContains(value, _root);
+		return _dateDelivery;
 	}
 
-	private:
-
-	bool IsContains(int value, Node*& node)
+	string GetInfo()
 	{
-		if(node)
-		{
-			return (value >= node->value) ? IsContains(value, node->Right) : IsContains(value, node->Left);
-		}
-		return false;
-	}
-
-	Node* AddNode(int value, Node* node)
-	{
-		if(node == NULL)
-		{
-			node  = new Node(value);
-		}
-		else
-		{
-			if(value >= node->value)
-			{
-				node->Right = AddNode(value, node->Right);
-			}
-			else
-			{
-				node->Left = AddNode(value, node->Left);
-			}
-		}
-		return node;
-	}
-
-	void ShowNode(Node* node, size_t whitespace = 0)
-	{
-		if(node)
-		{
-			ShowNode(node->Left, ++whitespace);
-			for(size_t i = 0; i < whitespace; i++)
-			{
-				cout << "\t";
-			}
-			cout << *node << '\n';
-			ShowNode(node->Right, ++whitespace);
-		}
+		cout << "Название: " << _name << ". "
+			<< "Дата поставки: " << _dateDelivery << ".\n";
 	}
 };
 
+class Employee
+{
+private:
+	string _fcs;
+	/// <summary>
+	/// формат ДД/ММ/ГГГГ
+	/// </summary>
+	string _dateOfBirth;
+	string _position;
 
+public:
+	Employee(): _fcs(string()), _dateOfBirth(string()), _position(string())
+	{}
+
+	Employee(string fcs, string dateOfBirth, string position)
+	{
+		_fcs = fcs;
+		_dateOfBirth = dateOfBirth;
+		_position = position;
+	}
+
+	string GetFcs()
+	{
+		return _fcs;
+	}
+
+	string GetDateOfBirth()
+	{
+		return _dateOfBirth;
+	}
+
+	string GetPosition()
+	{
+		return _position;
+	}
+
+	void Edit()
+	{
+		cout << "Изменить ФИО - 1" << endl;
+		cout << "Изменить Дату Рождения - 2" << endl;
+		cout << "Изменить Должность - 3" << endl;
+
+		int numberOfCommand;
+		cout << "Выберете номер команды: "; cin >> numberOfCommand;
+		string input;
+		switch(numberOfCommand)
+		{
+			case 1:
+			{
+				cout << "Введите новое ФИО: "; cin >> input;
+				_fcs = input;
+				break;
+			}
+			case 2:
+			{
+				cout << "Введите новую Дату Рождения(ДД/ММ/ГГГГ): "; cin >> input;
+				_dateOfBirth = input;
+				break;
+			}
+			case 3:
+			{
+				cout << "Введите новую Должность: "; cin >> input;
+				_dateOfBirth = input;
+				break;
+			}
+			default: cout << "Неизвестная команда!" << endl;
+				break;
+		}
+	}
+
+	string GetInfo()
+	{
+		cout << "ФИО: " << _fcs << ". "
+			<< "Дата рождения: " << _dateOfBirth << ". "
+			<< "Должность: " << _position << ".\n";
+	}
+};
+
+class Schedule
+{
+	string* _days;
+public:
+	Schedule()
+	{
+		_days = new string[7];
+	}
+
+	void Edit(size_t numberOfDay, string newDay)
+	{
+		_days[numberOfDay] = newDay;
+	}
+};
+
+class Warehouse
+{
+private:
+	vector<Employee> _employees;
+	vector<Product> _products;
+
+public:
+	Warehouse()
+	{
+		_employees = vector<Employee>();
+		_products = vector<Product>();
+	}
+
+	void AddEmployee(Employee employee)
+	{
+		_employees.push_back(employee);
+	}
+
+	void AddProduct(Product product)
+	{
+		_products.push_back(product);
+	}
+};
+
+void ReadFromFile()
+{
+
+}
 
 int main(int argc, char* argv[])
 {
-	BinaryTree tree;
-	srand(time(0));
 
-	unsigned int start_time = clock(); // начальное время
-	for(int i = 0; i < 9999999; i++)
-	{
-		tree.Add(1 + rand() % 999999);
-	}
-	unsigned int end_time = clock(); // конечное время
-	cout << "Create tree: " << (end_time - start_time) / 1000.0 << endl;;
+	setlocale(LC_ALL, "rus");
 
-
-	start_time = clock();
-	tree.Contains(999998);
-	end_time = clock(); // конечное время
-	cout << "Search: " << (end_time - start_time) / 1000.0 << endl;;
+	system("pause");
 	return 0;
 }
