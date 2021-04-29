e394d4262981a27291cf67f7b79657e66702a35e
max Icolumi <max77648@gmail.com>
2021-04-29 09:05:24 +0300

    Добавил генерацию случайно величины

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -6,7 +6,7 @@ void SolveHardMethod()
 	double a = 0;
 	for(int i = 0; i < 999999; i++)
 	{
-		for(int j = 0; j < 100; j++)
+		for(int j = 0; j < 10 + rand() % 99; j++) // генерация случайной величины
 		{
 			a = pow(a, j);
 		}
@@ -15,8 +15,12 @@ void SolveHardMethod()
 
 int main(int argc, char** argv)
 {
-
-	std::cout << GetTime(SolveHardMethod) << " sec."<< std::endl;
+	srand(time(0));
+	std::cout << GetTime(SolveHardMethod) << " sec." << std::endl;
+	std::cout << GetTime(SolveHardMethod) << " sec." << std::endl;
+	std::cout << GetTime(SolveHardMethod) << " sec." << std::endl;
+	std::cout << GetTime(SolveHardMethod) << " sec." << std::endl;
+	std::cout << GetTime(SolveHardMethod) << " sec." << std::endl;
 
 	return 0;
 }
